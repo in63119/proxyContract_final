ff77c463a89858556c8808b77620e91fc19c1d96
이인수 <in63119@gmail.com>
2023-02-07 21:57:41 +0900

    Fix Contract about tokenUri and MakeJson Logic

--- openzepplin/contracts/InNFT.sol
+++ openzepplin/contracts/InNFT.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract InNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract InNFT is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
@@ -19,14 +20,16 @@ contract InNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgr
 
     function initialize() initializer public {
         __ERC721_init("InNFT", "INFT");
+        __ERC721URIStorage_init();
         __Ownable_init();
         __UUPSUpgradeable_init();
     }
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
     function _authorizeUpgrade(address newImplementation)
@@ -34,4 +37,22 @@ contract InNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgr
         onlyOwner
         override
     {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 }
--- openzepplin/scripts/deploy.js
+++ openzepplin/scripts/deploy.js
@@ -5,22 +5,17 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const { makeInNFTData } = require("../data/makeData");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const InNFT = await hre.ethers.getContractFactory("InNFT");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const INFT = await upgrades.deployProxy(InNFT, { kind: "uups" });
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const proxyCA = INFT.address;
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Proxy Contract is deployed. address : ", proxyCA);
+  makeInNFTData(proxyCA);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
