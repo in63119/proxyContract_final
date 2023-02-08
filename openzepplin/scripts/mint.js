5b641b93e51947708d9c9c0fb292222555aac7e8
이인수 <in63119@gmail.com>
2023-02-08 22:38:14 +0900

    done mint logic

--- openzepplin/scripts/mint.js
+++ openzepplin/scripts/mint.js
@@ -1,18 +1,44 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
-const hre = require("hardhat");
-
 const { ADDRESS, PRIVATE_KEY, POLYGON_TEST } = process.env;
-const { InNFTCA, InNFTABI } = require("../data/getAbiData");
 
+const Web3 = require("web3");
+const web3 = new Web3(POLYGON_TEST);
+
+const { InNFTCA, InNFTABI } = require("../data/getAbiData");
 const tokenUri =
-  "https://gateway.pinata.cloud/ipfs/QmdSyMBmf5SaFxAsUuxCuwvL3iaW5SUXFKUPnZ7N5itG9p?_gl=1*19vjp7z*_ga*MTUzMTA0MTQzNC4xNjczNjE1Njcz*_ga_5RMPXG14TE*MTY3NTQ4NDM1Mi4yLjEuMTY3NTQ4NDM4NC4yOC4wLjA";
+  "https://gateway.pinata.cloud/ipfs/QmNqMaFXSZsss6tMBKcU7BY3mcmv3Ztv8nmM36zSFCqdjr";
 
 const minting = async () => {
-  const provider = new hre.ethers.providers.JsonRpcProvider(POLYGON_TEST);
-  const inNftContract = new hre.ethers.Contract(InNFTCA, InNFTABI, provider);
-  const safeMint = await inNftContract.safeMint(ADDRESS, tokenUri);
-  console.log(safeMint);
+  const InNftContract = new web3.eth.Contract(InNFTABI, InNFTCA).methods;
+  const mint = InNftContract.safeMint(ADDRESS, tokenUri).encodeABI();
+
+  const SendTransactionForValue = async (data, to) => {
+    await web3.eth.accounts
+      .signTransaction(
+        {
+          from: ADDRESS,
+          to: to,
+          gas: 5000000,
+          value: "0x0",
+          data: data,
+        },
+        PRIVATE_KEY
+      )
+      .then(async (Tx) => {
+        await web3.eth
+          .sendSignedTransaction(Tx.rawTransaction)
+          .then((hash, err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("success For Value");
+            }
+          });
+      });
+  };
+
+  await SendTransactionForValue(mint, InNFTCA);
 };
 
 minting();
